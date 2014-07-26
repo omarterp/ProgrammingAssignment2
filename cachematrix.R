b796c929a84e77a1177e1c9ad28eee1009faaefe
Eric Perales <omarterp@gmail.com>
2014-07-25 23:04:38 -0400

    Implemented makeCacheMatrix and cacheSolve functions.  makeCacheMatrix generates an object that will then be passed in to cacheSolve to generate the inverted matrix or pull the value from a global variable (cached).

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Maintains a special object which allows the assignee the ability to create an invertible matrix and cache it
 
-## Write a short comment describing this function
-
+## Creates a reference to the object for the given matrix and creates helper functions to set, cache and get its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  invMatrix <- NULL
+  
+  set <- function(y) {
+    
+    x <<- NULL
+    invMatrix <<- NULL
+  }
+  
+  get <- function() x
+  setInverted <- function(invertedMatrix) invMatrix <<- invertedMatrix
+  getInverted <- function() invMatrix
+  
+  list(get = get, set = set, getInverted = getInverted, setInverted = setInverted)
 }
 
+## Returns the given matrix inverted. An equality check is performed on a cached global variable.
 
-## Write a short comment describing this function
+## If the special matrix already has its inverted value cached, then it is returned from the global
+## variable - invertedMatrix.
 
+## I chose to check for the null value and change flow to proceed to the last line of the function, 
+## instead of using a return before the function block ends. The function flows better this way, in my opinion.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  invMatrix <- x$getInverted()
+  
+  if(is.null(invMatrix)) {
+    
+    print("Setting inverted Value.")
+    
+    invertedMatrix <- solve(myMatrix, ...)
+    invMatrix <- invertedMatrix
+    x$setInverted(invMatrix)
+    
+  } else {
+    print("Getting inverted matrix from cache.")
+    
+    invMatrix <- x$getInverted()
+  }
+  
+  invMatrix
 }
